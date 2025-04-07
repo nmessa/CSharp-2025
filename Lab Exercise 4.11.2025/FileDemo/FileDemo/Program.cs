c0171f431918ecec97b2b03b095ce79178ad5ab4
nmessa <nmessa@sau16.org>
2025-04-07 10:31:36 -0400

    Add files via upload

---
+++ Lab Exercise 4.11.2025/FileDemo/FileDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create FileStream and StreamWriter
+            FileStream fs = new FileStream("data.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter textfile = new StreamWriter(fs);
+
+            textfile.WriteLine("This is line 1");
+            textfile.WriteLine("This is line 2");
+            textfile.WriteLine("This is line 3");
+
+            //Close StreamWriter and FileStream
+            textfile.Close();
+            fs.Close();
+
+            //Create FileStream and StreamReader
+            FileStream fs2 = new FileStream("data.txt", FileMode.Open, FileAccess.Read);
+            StreamReader textfile2 = new StreamReader(fs2);
+
+            while (!textfile2.EndOfStream)
+            {
+                Console.WriteLine(textfile2.ReadLine());
+            }
+
+            //Close StreamReader and FileStream
+            textfile.Close();
+            fs.Close();
+
+        }
+    }
+}
