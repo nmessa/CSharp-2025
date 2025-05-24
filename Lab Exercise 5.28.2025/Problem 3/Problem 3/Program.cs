cac75275ad811791cd5c5138c2253f293dc6e37f
nmessa <nmessa@sau16.org>
2025-05-24 10:32:37 -0400

    Add files via upload

---
+++ Lab Exercise 5.28.2025/Problem 3/Problem 3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Lab Exercise 5.28.2025 Problem 3
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(functionThree("Python"));
+            Console.WriteLine(functionThree("ytade"));
+            Console.WriteLine(functionThree("jsues"));
+        }
+
+        static string functionThree(string s)
+        {
+            //Add code here
+
+        }
+    }
+}
---
+++ Lab Exercise 5.28.2025/Problem 4/Problem 4/Program.cs
@@ -0,0 +1,26 @@
+﻿//Lab Exercise 5.28.2025 Problem 4
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(functionFour("Python"));
+            Console.WriteLine(functionFour("PHP"));
+            Console.WriteLine(functionFour("JS"));
+        }
+
+        static string functionFour(string s)
+        {
+            //Add code here
+
+        }
+    }
+}
