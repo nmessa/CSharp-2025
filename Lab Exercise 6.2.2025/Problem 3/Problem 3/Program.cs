57dcba8b577c3d63d554826fd8d621dd98a6ca94
nmessa <nmessa@sau16.org>
2025-05-30 13:48:39 -0400

    Add files via upload

---
+++ Lab Exercise 6.2.2025/Problem 3/Problem 3/Program.cs
@@ -0,0 +1,31 @@
+﻿//Lab Exercise 6.2.2025 Problem 3
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(functionThree("Python", 1));
+            Console.WriteLine(functionThree("Python", 0));
+            Console.WriteLine(functionThree("Python", 4));
+        }
+
+        static string functionThree(string s, int n)
+        {
+            //Add code here
+
+        }
+    }
+}
+
+//Output
+//Pthon
+//ython
+//Pythn
